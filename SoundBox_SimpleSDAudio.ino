97a5130af53e59b79882991dee3c5667600644b8
ReDiculum <rediculum0@runlevel.ch>
2019-08-19 20:47:43 +0200

    :traktor: refactor code and improve

--- SoundBox_SimpleSDAudio.ino
+++ SoundBox_SimpleSDAudio.ino
@@ -1,10 +1,11 @@
 /*
-=============================================================================================
+==========================================================================
 Sound Box 1.0
 by ReDiculum (Aug - Sep 2019)
 -------------------------------
-Sound box like a board to launch samples stored as 8bit full-rate (62500) mono PCM files named from 1.PCM to 10.PCM
-from arcade buttons using a demux
+Sound box like a board to launch samples stored as 
+8bit full-rate (62500) mono PCM files named from 1.PCM to 10.PCM
+from arcade buttons using a multiplexer
 
 SD Card > Arduino pinout:
 
@@ -13,43 +14,45 @@ CS (1)   -> Pin 10 (Chip select)
 MOSI (2) -> Pin 11
 MISO (7) -> Pin 12
 
-Speaker (sound) -> Pin 9 and GND
+Speaker (sound) -> Pin 6 and GND
 
-CD74HC4067 > Arduino pinout:
-S0 -> Pin 7
-S1 -> Pin 6
-S2 -> Pin 5
-S3 -> Pin 4
+Multiplexer > Arduino pinout:
+S0 -> Pin 2
+S1 -> Pin 3
+S2 -> Pin 4
+S3 -> Pin 5 if you have a CD74HC4067 and provide 16 arcade buttons
 SIG -> Pin A0
 
-=============================================================================================
-
-https://www.youtube.com/watch?v=OgaeEiHemU4
+============================================================================
 
 SimpleSDAudio library for audio playback from SD card
 http://www.hackerspace-ffm.de/wiki/index.php?title=SimpleSDAudio
 
+How to deal with mux and demux
+https://learn.sparkfun.com/tutorials/multiplexer-breakout-hookup-guide/all
+
 */
 #include <SimpleSDAudio.h>
 
-#define SPEAKER_PIN 9
+// Pin assignment
+#define SPEAKER_PIN 6
 #define SD_CHIPSELECT_PIN 10
 #define LED_PIN 13 // onboard LED
-#define S0_PIN 7
-#define S1_PIN 6
-#define S2_PIN 5
-#define S3_PIN 4 
-#define SIG_PIN 0
+#define SIG_PIN 14 // A0
+
+#define BUTTONS 8 // How many acrade buttons
+//#define BUTTONS 16 
 
 void setup() {
+  // Define your S-pins from your multiplexer here. 
+  const int selectPins[3] = {2,3,4}; 
+  //const int selectPins[4] = {2,3,4,5}; // CD74HC4067
+
   Serial.begin(57600); // Set console to 57600 baud
   Serial.println("=== SoundBox based on SSDA ===");  
 
   pinMode(LED_PIN, OUTPUT);
-  pinMode(S0_PIN, OUTPUT);
-  pinMode(S1_PIN, OUTPUT);
-  pinMode(S2_PIN, OUTPUT);
-  pinMode(S3_PIN, OUTPUT);
+  byte s_pins = sizeof(selectPins);
 
   // Set Chip Select Pin
   SdPlay.setSDCSPin(SD_CHIPSELECT_PIN);
@@ -62,29 +65,53 @@ void setup() {
 }
 
 void loop(void) { 
-  int countImpulse=1;
+  // Loop through all eight pins holding an arcade button.
+  for (byte button=0; button<=BUTTONS; button++)
+  {
+    selectMuxPin(pin, s_pins);
+    // read Z and reduce value
+    byte launch = map(analogRead(SIG_PIN),0,1023,0,1); 
 
-  /* define a char array with an index size of 6 and convert
-  integer "countImpulse" to char "track" using utoa() function
-  http://www.nongnu.org/avr-libc/user-manual/group__avr__stdlib.html 
-  */
-  char track[6]; 
-  utoa(countImpulse,track,10);
-  // Append the suffix .PCM to the char array
-  strcat(track, ".PCM");
-  
-  // Set the audio file if it exists and play it
-  if (SdPlay.setFile(track)) {
-    Serial.print("Playing..."); Serial.println(track);
-    SdPlay.play();
-    while(!SdPlay.isStopped()) {
-      ;
+    if (launch == 1) {
+      Serial.println();
+      delay(100);
+
+      /* define a char array with an index size of 6 and convert
+      integer "countImpulse" to char "track" using utoa() function
+      http://www.nongnu.org/avr-libc/user-manual/group__avr__stdlib.html 
+      */
+      char track[6]; 
+      utoa(button,track,10);
+      // Append the suffix .PCM to the char array
+      strcat(track, ".PCM");
+      
+      // Set the audio file if it exists and play it
+      if (SdPlay.setFile(track)) {
+        Serial.print("Playing..."); Serial.println(track);
+        SdPlay.play();
+        while(!SdPlay.isStopped()) {
+          ;
+        }
+        SdPlay.deInit();
+      } else {
+        SdPlay.deInit();
+        Serial.print("File "); Serial.print(track); Serial.println(" not found");
+        tone(SPEAKER_PIN,80); delay(250); noTone(SPEAKER_PIN); delay(400);
+        tone(SPEAKER_PIN,120); delay(600); noTone(SPEAKER_PIN);
+      }
     }
-    SdPlay.deInit();
-  } else {
-    SdPlay.deInit();
-    Serial.print("File "); Serial.print(track); Serial.println(" not found");
-    tone(SPEAKER_PIN,80); delay(250); noTone(SPEAKER_PIN); delay(400);
-    tone(SPEAKER_PIN,120); delay(600); noTone(SPEAKER_PIN);
+  }
+}
+
+// The selectMuxPin function sets the S-pins
+// accordingly, given a pin from 0-7.
+void selectMuxPin(byte pin, byte pins)
+{
+  for (int i=0; i<pins; i++)
+  {
+    if (pin & (1<<i))
+      digitalWrite(selectPins[i], HIGH);
+    else
+      digitalWrite(selectPins[i], LOW);
   }
 }
